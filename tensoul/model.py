4fdd40c5c2c40e191141d3a951d753669b35d416
Steven Vch <unstatik@staremax.com>
2023-11-04 02:47:13 +0300

    fix decode seat wind

--- tensoul/model.py
+++ tensoul/model.py
@@ -263,7 +263,7 @@ class Yaku:
 
     def name(self, round: Round, seat: int) -> str:
         if self.id == 10:
-            return f"{RUNES['jikaze'][JPNAME]} {RUNES[self.WIND[(seat + round.kyoku) % 4]][JPNAME]}"
+            return f"{RUNES['jikaze'][JPNAME]} {RUNES[self.WIND[round.kyoku - 1]][JPNAME]}"
         if self.id == 11:
             return f"{RUNES['bakaze'][JPNAME]} {RUNES[self.WIND[round.kyoku // 4]][JPNAME]}"
         elif self.id == 18:
